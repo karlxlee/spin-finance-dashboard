791bed0d4b5893561b76d2a2bedd41633fc3f811
Karl Lee <poogooflupduck@gmail.com>
2022-02-10 14:10:15 +0000

    add pg as dialectmodule

--- utils/indexer.js
+++ utils/indexer.js
@@ -1,8 +1,10 @@
 import { Sequelize, QueryTypes } from "sequelize";
+import pg from "pg"
 import config from "../config.json";
 
 const indexer = new Sequelize(config["indexer_uri"], {
   dialect: "postgres",
+  dialectModule: pg,
 });
 (async () => {
   await indexer.authenticate();
