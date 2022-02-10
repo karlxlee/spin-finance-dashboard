2a6a072bf494f9d085c053f5c60438c6a9de800a
Karl Lee <poogooflupduck@gmail.com>
2022-02-10 14:02:00 +0000

    debug indexer init api route

--- pages/api/orderCount.js
+++ pages/api/orderCount.js
@@ -49,6 +49,7 @@ export async function orderCount({
 }
 
 export default async function handler(req, res) {
+  await indexer.authenticate();
   const { query } = req;
   try {
     const result = await orderCount({ ...query });
